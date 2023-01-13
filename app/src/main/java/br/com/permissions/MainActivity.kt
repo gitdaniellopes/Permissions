9b469f3b466cf82a1e0a91d5bc305277d10e0a95
Daniel Lopes <danieljfsantos@gmail.com>
2023-01-13 10:46:51 -0300

    Initial commit

---
+++ app/src/main/java/br/com/permissions/MainActivity.kt
@@ -0,0 +1,52 @@
+package br.com.permissions
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.com.permissions.ui.theme.PermissionsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PermissionsTheme {
+                val navController = rememberNavController()
+                val scaffoldState = rememberScaffoldState()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
+                ) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.MAIN
+                    ) {
+                        composable(Route.MAIN) {
+                            MainScreen(navController)
+                        }
+
+                        composable(Route.DEFAULT) {
+                            BuiltInPermissionsScreen()
+                        }
+
+                        composable(Route.ACCOMPANIST) {
+                            AccompanistPermissionsScreen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+object Route {
+    const val MAIN = "main"
+    const val DEFAULT = "default"
+    const val ACCOMPANIST = "accompanist"
+}
